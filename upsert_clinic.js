be23628bf340e3518e1e36bdaf7e20a61556fb79
Taylor Downs <taylor@openfn.org>
2018-02-06 14:56:20 -0300

    Update upsert_clinic.js

--- upsert_clinic.js
+++ upsert_clinic.js
@@ -1,6 +1,7 @@
 // =============================================================================
 // Upsert Clinic records in Salesforce when Location "Hospital" records are
 // updated in Commcare.
+// test, feb 6th
 // =============================================================================
 upsert("clinic__c", "CAST Location ID", fields(
   field('CAST Location ID', dataValue('properties.location_id')),
