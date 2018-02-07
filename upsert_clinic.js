c27452523cc2e965f24b1d5bc7c40a7c3fc0345c
skz828 <stephanie.zimmerman@miraclefeet.org>
2018-02-07 10:47:42 -0500

    Added SF CommCare Location ID

--- upsert_clinic.js
+++ upsert_clinic.js
@@ -3,8 +3,8 @@
 // updated in Commcare.
 // test, feb 6th
 // =============================================================================
-upsert("gciclubfoot__Clinic__c", "CAST Location ID", fields(
-  field('CAST Location ID', dataValue('properties.location_id')),
+upsert("gciclubfoot__Clinic__c", "gciclubfoot__CAST_Location_ID", fields(
+  field('gciclubfoot__CAST_Location_ID', dataValue('properties.location_id')),
   field('CAST Site Code', dataValue('properties.site_code')),
   field('Name', dataValue('properties.name')),
   field('CAST Parent Site Code', dataValue('properties.parent_site_code'))
