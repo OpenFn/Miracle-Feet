7255babef9c208c166457250d6fd7baea85d7267
Taylor Downs <taylor@openfn.org>
2018-02-14 22:33:35 -0300

    taylor@openfn.org OpenFn Web Edit

--- upsert_visit.js
+++ upsert_visit.js
@@ -2,10 +2,10 @@
 // Upsert Visit records in Salesforce when "Visit" cases are updated in CC.
 // =============================================================================
 upsert("gciclubfoot__Visit__c", "gciclubfoot__commcare_case_id__c", fields(
-  field('gciclubfoot__commcare_case_id__c', dataValue('properties.caseid')),
+  field('gciclubfoot__commcare_case_id__c', dataValue('case_id')),
   // do not link to clinic for the time being, only link to patient...
   // relationship('Hospital__r', "uuid__c", dataValue('properties.hospital_code')),
-  relationship('gciclubfoot__Patient__r', "gciclubfoot__CommCare_Case_ID__c", dataValue('indices.parent.caseid')),
+  relationship('gciclubfoot__Patient__r', "gciclubfoot__CommCare_Case_ID__c", dataValue('indices.parent.case_id')),
   field('gciclubfoot__Visit_Date__c', dataValue('properties.visit_date')),
   // field('gciclubfoot__Patient__c', dataValue('properties.patient_id')),
   field('gciclubfoot__Brace_Count__c', dataValue('properties.brace_count')),
