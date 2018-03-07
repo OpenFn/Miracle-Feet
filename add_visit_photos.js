00c86fb94adb304c2cdfb2add4cf49af1dc4cd67
skz828 <stephanie.zimmerman@miraclefeet.org>
2018-03-07 12:58:22 -0500

    Updated API Name for photo 1, added photo 2-3

--- add_visit_photos.js
+++ add_visit_photos.js
@@ -1,6 +1,16 @@
 upsert("gciclubfoot__Visit__c", "gciclubfoot__commcare_case_id__c", fields(
   field('gciclubfoot__commcare_case_id__c', dataValue('form.case.@case_id')),
-  field("Photo_1__c", function(state) {
+  field("Photo_1_URL__c", function(state) {
+    const baseUrl = "https://www.commcarehq.org/a/clubfoot/api/form/attachment/";
+    const uuid = state.data.metadata.instanceID;
+    return `${baseUrl}${uuid}/${state.data.form.photos.photo1}`
+  })
+  field("Photo_2_URL__c", function(state) {
+    const baseUrl = "https://www.commcarehq.org/a/clubfoot/api/form/attachment/";
+    const uuid = state.data.metadata.instanceID;
+    return `${baseUrl}${uuid}/${state.data.form.photos.photo1}`
+  })
+field("Photo_3_URL__c", function(state) {
     const baseUrl = "https://www.commcarehq.org/a/clubfoot/api/form/attachment/";
     const uuid = state.data.metadata.instanceID;
     return `${baseUrl}${uuid}/${state.data.form.photos.photo1}`
