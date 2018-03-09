6bb21330afdf140f840f076269630f244373ef21
skz828 <stephanie.zimmerman@miraclefeet.org>
2018-03-09 12:13:35 -0500

    Added gciclubfoot label

--- add_patient_photos.js
+++ add_patient_photos.js
@@ -1,21 +1,21 @@
 upsert("gciclubfoot__Patient__c", "gciclubfoot__CommCare_Case_ID__c", fields(
   field('gciclubfoot__CommCare_Case_ID__c', dataValue("form.case.@case_id")),
-  field("Registration_Photo_1__c", function(state) {
+  field("gciclubfoot__Registration_Photo_1__c", function(state) {
     const baseUrl = "https://www.commcarehq.org/a/clubfoot/api/form/attachment/";
     const uuid = state.data.metadata.instanceID;
     return `${baseUrl}${uuid}/${state.data.form.photos.photo1}`
   }),
-  field("Registration_Photo_2__c", function(state) {
+  field("gciclubfoot__Registration_Photo_2__c", function(state) {
     const baseUrl = "https://www.commcarehq.org/a/clubfoot/api/form/attachment/";
     const uuid = state.data.metadata.instanceID;
     return `${baseUrl}${uuid}/${state.data.form.photos.photo2}`
   }),
-  field("Registration_Photo_3__c", function(state) {
+  field("gciclubfoot__Registration_Photo_3__c", function(state) {
     const baseUrl = "https://www.commcarehq.org/a/clubfoot/api/form/attachment/";
     const uuid = state.data.metadata.instanceID;
     return `${baseUrl}${uuid}/${state.data.form.photos.photo3}`
   }),
-  field("Registration_Photo_4__c", function(state) {
+  field("gciclubfoot__Registration_Photo_4__c", function(state) {
     const baseUrl = "https://www.commcarehq.org/a/clubfoot/api/form/attachment/";
     const uuid = state.data.metadata.instanceID;
     return `${baseUrl}${uuid}/${state.data.form.photos.photo4}`
