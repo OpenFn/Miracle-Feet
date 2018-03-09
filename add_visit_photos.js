ba20d3b144536fbd3bb587ab0e5bf2ca08d7812b
Taylor Downs <downs.taylor@gmail.com>
2018-03-09 17:54:29 +0000

    relate visit photos to patient

--- add_visit_photos.js
+++ add_visit_photos.js
@@ -1,5 +1,6 @@
 upsert("gciclubfoot__Visit__c", "gciclubfoot__commcare_case_id__c", fields(
   field('gciclubfoot__commcare_case_id__c', dataValue('form.case.@case_id')),
+  relationship('gciclubfoot__Patient__r', "gciclubfoot__CommCare_Case_ID__c", dataValue('form.subcase_0.case.@case_id')),
   field("gciclubfoot__Photo_1_URL__c", function(state) {
     const baseUrl = `https://www.commcarehq.org/a/${state.data.domain}/api/form/attachment/`;
     const uuid = state.data.metadata.instanceID;
