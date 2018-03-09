53189e1e0ff91b25c33d97079973ba0ad9207f57
Taylor Downs <downs.taylor@gmail.com>
2018-03-09 17:00:35 +0000

    added commas in photo jobs

--- add_visit_photos.js
+++ add_visit_photos.js
@@ -4,13 +4,13 @@ upsert("gciclubfoot__Visit__c", "gciclubfoot__commcare_case_id__c", fields(
     const baseUrl = "https://www.commcarehq.org/a/clubfoot/api/form/attachment/";
     const uuid = state.data.metadata.instanceID;
     return `${baseUrl}${uuid}/${state.data.form.photos.photo1}`
-  })
+  }),
   field("Photo_2_URL__c", function(state) {
     const baseUrl = "https://www.commcarehq.org/a/clubfoot/api/form/attachment/";
     const uuid = state.data.metadata.instanceID;
     return `${baseUrl}${uuid}/${state.data.form.photos.photo1}`
-  })
-field("Photo_3_URL__c", function(state) {
+  }),
+  field("Photo_3_URL__c", function(state) {
     const baseUrl = "https://www.commcarehq.org/a/clubfoot/api/form/attachment/";
     const uuid = state.data.metadata.instanceID;
     return `${baseUrl}${uuid}/${state.data.form.photos.photo1}`
