878a85579d91a24a7083b03f725e937fedecab48
Taylor Downs <downs.taylor@gmail.com>
2018-03-09 17:00:35 +0000

    added commas in photo jobs

--- add_patient_photos.js
+++ add_patient_photos.js
@@ -4,22 +4,22 @@ upsert("gciclubfoot__Patient__c", "gciclubfoot__CommCare_Case_ID__c", fields(
     const baseUrl = "https://www.commcarehq.org/a/clubfoot/api/form/attachment/";
     const uuid = state.data.metadata.instanceID;
     return `${baseUrl}${uuid}/${state.data.form.photos.photo1}`
-  })
-    field("Registration_Photo_2__c", function(state) {
+  }),
+  field("Registration_Photo_2__c", function(state) {
     const baseUrl = "https://www.commcarehq.org/a/clubfoot/api/form/attachment/";
     const uuid = state.data.metadata.instanceID;
     return `${baseUrl}${uuid}/${state.data.form.photos.photo2}`
-  })
+  }),
   field("Registration_Photo_3__c", function(state) {
     const baseUrl = "https://www.commcarehq.org/a/clubfoot/api/form/attachment/";
     const uuid = state.data.metadata.instanceID;
     return `${baseUrl}${uuid}/${state.data.form.photos.photo3}`
-  })
+  }),
   field("Registration_Photo_4__c", function(state) {
     const baseUrl = "https://www.commcarehq.org/a/clubfoot/api/form/attachment/";
     const uuid = state.data.metadata.instanceID;
     return `${baseUrl}${uuid}/${state.data.form.photos.photo4}`
-  })
+  }),
   field("gciclubfoot__Consent_Signature__c", function(state) {
     const baseUrl = "https://www.commcarehq.org/a/clubfoot/api/form/attachment/";
     const uuid = state.data.metadata.instanceID;
