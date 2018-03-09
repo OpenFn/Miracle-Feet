9d493bb6cad5307ec54286d8fa1988e2f85b31f0
Taylor Downs <downs.taylor@gmail.com>
2018-03-09 17:59:38 +0000

    only set photoUrl if image exists

--- add_visit_photos.js
+++ add_visit_photos.js
@@ -4,16 +4,19 @@ upsert("gciclubfoot__Visit__c", "gciclubfoot__commcare_case_id__c", fields(
   field("gciclubfoot__Photo_1_URL__c", function(state) {
     const baseUrl = `https://www.commcarehq.org/a/${state.data.domain}/api/form/attachment/`;
     const uuid = state.data.metadata.instanceID;
-    return `${baseUrl}${uuid}/${state.data.form.photos.photo1}`
+    const image = state.data.form.photos.photo1;
+    return ( image ? `${baseUrl}${uuid}/${image}` : "" )
   }),
   field("gciclubfoot__Photo_2_URL__c", function(state) {
     const baseUrl = `https://www.commcarehq.org/a/${state.data.domain}/api/form/attachment/`;
     const uuid = state.data.metadata.instanceID;
-    return `${baseUrl}${uuid}/${state.data.form.photos.photo1}`
+    const image = state.data.form.photos.photo2;
+    return ( image ? `${baseUrl}${uuid}/${image}` : "" )
   }),
   field("gciclubfoot__Photo_3_URL__c", function(state) {
     const baseUrl = `https://www.commcarehq.org/a/${state.data.domain}/api/form/attachment/`;
     const uuid = state.data.metadata.instanceID;
-    return `${baseUrl}${uuid}/${state.data.form.photos.photo1}`
+    const image = state.data.form.photos.photo3;
+    return ( image ? `${baseUrl}${uuid}/${image}` : "" )
   })
 ));
