6816931d0353602371611f17d09080352706da23
Taylor Downs <downs.taylor@gmail.com>
2018-03-13 20:38:37 +0000

    port visit details from CASE to work on FORM submission

---
+++ add_new_visit.js
@@ -0,0 +1,200 @@
+upsert("gciclubfoot__Visit__c", "gciclubfoot__commcare_case_id__c", fields(
+  field('gciclubfoot__commcare_case_id__c', dataValue('form.subcase_0.case.@case_id')),
+  relationship('gciclubfoot__Patient__r', "gciclubfoot__CommCare_Case_ID__c", dataValue('form.case.@case_id')),
+  field('gciclubfoot__Visit_Date__c', (state) => {
+    const validDate = state.data.form.subcase_0.case.update.visit_date
+    return ( validDate ? new Date(validDate).toISOString() : null )
+  }),
+
+  //Brace Questions ============================================================
+  field('gciclubfoot__Brace_Count__c', dataValue('form.subcase_0.case.update.brace_count')),
+  field('gciclubfoot__Brace_Problems__c', humanProper(state.data.form.subcase_0.case.update.brace_problems)), // picklist
+  field('gciclubfoot__Brace_Problems_Notes__c', dataValue('form.subcase_0.case.update.brace_problems_specified')),
+  field('gciclubfoot__Brace_Problems_Type__c', (state) => {
+    const ms = state.data.form.subcase_0.case.update.brace_problems_type
+    if (ms) {
+      return ms.replace(/ /gi, ';').toLowerCase().split(';').map((value) => {
+        return humanProper(value)
+      }).join(';');
+    } else { return "" }
+  }),
+  // ===========================================================================
+
+  field('gciclubfoot__Brace_Type__c', humanProper(state.data.form.subcase_0.case.update.brace_type)), // picklist
+  field('gciclubfoot__Brace_Condition_Non_MiracleFeet_Brace__c', humanProper(state.data.form.subcase_0.case.update.brace_condition)), // picklist
+  field('gciclubfoot__MiracleFeet_Bar_Condition__c', humanProper(state.data.form.subcase_0.case.update.miraclefeet_bar_condition)), // picklist
+  field('gciclubfoot__MiracleFeet_Bar_Size__c', humanProper(state.data.form.subcase_0.case.update.miraclefeet_bar_size)), // picklist
+  field('gciclubfoot__MiracleFeet_Shoe_Size__c', humanProper(state.data.form.subcase_0.case.update.miraclefeet_shoe_size)), // picklist
+  field('gciclubfoot__MiracleFeet_Brace_Given__c', humanProper(state.data.form.subcase_0.case.update.miraclefeet_brace_given)), // picklist
+  field('gciclubfoot__MiracleFeet_Shoes_Condition__c', humanProper(state.data.form.subcase_0.case.update.miraclefeet_shoes_condition)), // picklist
+
+
+  // TODO: confirm that we can safely remove this ==============================
+  // field('gciclubfoot__Case_Closed_by_Username__c', humanProper(state.data.form.subcase_0.case.update.closed_by_username)), // picklist
+  // field('gciclubfoot__Case_Closed__c', (state) => {
+  //   return (state.data.form.subcase_0.case.update.closed == "1" ? true : false) // sf checkbox
+  // }),
+  // field('gciclubfoot__Case_Closed_Date__c', (state) => {
+  //   const validDate = state.data.form.subcase_0.case.update.date_closed
+  //   return ( validDate ? new Date(validDate).toISOString() : null )
+  // }),
+  // ===========================================================================
+  field('gciclubfoot__Cast_Count__c', dataValue('form.subcase_0.case.update.cast_count')),
+  field('gciclubfoot__Casting_Complications_Type__c', (state) => {
+    const ms = state.data.form.subcase_0.case.update.complication_type
+    if (ms) {
+      return ms.replace(/ /gi, ';').toLowerCase().split(';').map((value) => {
+        return humanProper(value)
+      }).join(';');
+    } else { return "" }
+  }),
+  field('gciclubfoot__Casting_Complications_Notes__c', dataValue('form.subcase_0.case.update.complication_type_other')),
+  field('gciclubfoot__Date_Referral_Made__c', (state) => {
+    const validDate = state.data.form.subcase_0.case.update.date_referral_made
+    return ( validDate ? new Date(validDate).toISOString() : null )
+  }),
+  field('gciclubfoot__Date_of_Tenotomy__c', (state) => {
+    const validDate = state.data.form.subcase_0.case.update.date_tenotomy
+    return ( validDate ? new Date(validDate).toISOString() : null )
+  }),
+  field('gciclubfoot__First_Brace__c', (state) => {
+    return (state.data.form.subcase_0.case.update.is_first_brace == "1" ? true : false) // sf checkbox
+  }),
+  field('gciclubfoot__ICR_ID__c', dataValue('form.subcase_0.case.update.visit_original_id')),
+  field('gciclubfoot__Left_Angle_of_Abduction__c', dataValue('form.subcase_0.case.update.l_angle_abduction')),
+  field('gciclubfoot__Left_Angle_of_Dorsiflexion__c', dataValue('form.subcase_0.case.update.l_angle_dorsiflexion')),
+  field('gciclubfoot__Left_Medial_Crease__c', dataValue('form.subcase_0.case.update.l_medial_crease')),
+  field('gciclubfoot__Left_Talar_Head__c', dataValue('form.subcase_0.case.update.l_talar_head')),
+  field('gciclubfoot__Left_Curved_Lateral_Border__c', dataValue('form.subcase_0.case.update.l_curved_lateral_border')),
+  field('gciclubfoot__Left_Midfoot_Score__c', dataValue('form.subcase_0.case.update.l_midfoot_score')),
+  field('gciclubfoot__Left_Posterior_Crease__c', dataValue('form.subcase_0.case.update.l_posterior_crease')),
+  field('gciclubfoot__Left_Empty_Heel__c', dataValue('form.subcase_0.case.update.l_empty_heel')),
+  field('gciclubfoot__Left_Rigid_Equinus__c', dataValue('form.subcase_0.case.update.l_rigid_equinus')),
+  field('gciclubfoot__Left_Hindfoot_Score__c', dataValue('form.subcase_0.case.update.l_hindfoot_score')),
+  field('gciclubfoot__Left_Pirani_Total_Score__c', dataValue('form.subcase_0.case.update.l_total_score')),
+  field('gciclubfoot__Left_Pirani_Score_Improved__c', dataValue('form.subcase_0.case.update.l_score_improved')),
+  field('gciclubfoot__Left_Pirani_Score_Not_Improved__c', dataValue('form.subcase_0.case.update.l_score_not_improved')),
+  field('gciclubfoot__Left_Pirani_Score_Same__c', dataValue('form.subcase_0.case.update.l_score_same')),
+  field('gciclubfoot__Left_Treatment__c', humanProper(state.data.form.subcase_0.case.update.l_treatment)), // picklist
+  field('gciclubfoot__Left_Treatment_Other__c', dataValue('form.subcase_0.case.update.l_treatment_other')),
+  field('gciclubfoot__Left_Surgery_Type__c', humanProper(state.data.form.subcase_0.case.update.l_surgery_type)), // picklist
+  field('gciclubfoot__Left_Surgery_Type_Other__c', dataValue('form.subcase_0.case.update.l_surgery_type_other')),
+  field('gciclubfoot__Right_Angle_of_Abduction__c', dataValue('form.subcase_0.case.update.r_angle_abduction')),
+  field('gciclubfoot__Right_Angle_of_Dorsiflexion__c', dataValue('form.subcase_0.case.update.r_angle_dorsiflexion')),
+  field('gciclubfoot__Right_Medial_Crease__c', dataValue('form.subcase_0.case.update.r_medial_crease')),
+  field('gciclubfoot__Right_Talar_Head__c', dataValue('form.subcase_0.case.update.r_talar_head')),
+  field('gciclubfoot__Right_Curved_Lateral_Border__c', dataValue('form.subcase_0.case.update.r_curved_lateral_border')),
+  field('gciclubfoot__Right_Midfoot_Score__c', dataValue('form.subcase_0.case.update.r_midfoot_score')),
+  field('gciclubfoot__Right_Posterior_Crease__c', dataValue('form.subcase_0.case.update.r_posterior_crease')),
+  field('gciclubfoot__Right_Empty_Heel__c', dataValue('form.subcase_0.case.update.r_empty_heel')),
+  field('gciclubfoot__Right_Rigid_Equinus__c', dataValue('form.subcase_0.case.update.r_rigid_equinus')),
+  field('gciclubfoot__Right_Hindfoot_Score__c', dataValue('form.subcase_0.case.update.r_hindfoot_score')),
+  field('gciclubfoot__Right_Total_Pirani_Score__c', dataValue('form.subcase_0.case.update.r_total_score')),
+  field('gciclubfoot__Right_Pirani_Score_Improved__c', dataValue('form.subcase_0.case.update.r_score_improved')),
+  field('gciclubfoot__Right_Pirani_Score_Not_Improved__c', dataValue('form.subcase_0.case.update.r_score_not_improved')),
+  field('gciclubfoot__Right_Pirani_Score_Same__c', dataValue('form.subcase_0.case.update.r_score_same')),
+  field('gciclubfoot__Right_Treatment__c', humanProper(state.data.form.subcase_0.case.update.r_treatment)), // picklist
+  field('gciclubfoot__Right_Treatment_Other__c', dataValue('form.subcase_0.case.update.r_treatment_other')),
+  field('gciclubfoot__Right_Surgery_Type__c', humanProper(state.data.form.subcase_0.case.update.r_surgery_type)),
+  field('gciclubfoot__Right_Surgery_Type_Other__c', dataValue('form.subcase_0.case.update.r_surgery_type_other')),
+  field('gciclubfoot__New_Brace__c', dataValue('form.subcase_0.case.update.is_new_brace')),
+  field('gciclubfoot__Next_Visit_Date__c', (state) => {
+    const validDate = state.data.form.subcase_0.case.update.next_visit_date
+    return ( validDate ? new Date(validDate).toISOString() : null )
+  }),
+
+  // TODO: confirm that this is submitted by and submitted date ================
+  // and we can safely remove last_modified and last_modified_by
+  field('gciclubfoot__Opened_By_Username_CommCare__c', dataValue('form.subcase_0.case.update.opened_by_username')),
+  field('gciclubfoot__Opened_Date_CommCare__c', (state) => {
+    const validDate = state.data.form.subcase_0.case.update.date_opened
+    return ( validDate ? new Date(validDate).toISOString() : null )
+  }),
+  // field('gciclubfoot__Last_Modified_By_Username_CommCare__c', dataValue('last_modified_by_user_username')),
+  // field('gciclubfoot__Last_Modified_Date_CommCare__c', (state) => {
+    // const validDate = state.data.date_modified
+    // return ( validDate ? new Date(validDate).toISOString() : null )
+  // }),
+  // ===========================================================================
+
+  field('gciclubfoot__Owner_Name_CommCare__c', dataValue('form.subcase_0.case.update.owner_name')),
+
+  //Relapse questions ==========================================================
+  field('gciclubfoot__Relapse__c', humanProper(state.data.form.subcase_0.case.update.recurrence)), // picklist
+  field('gciclubfoot__Relapse_Count__c', dataValue('form.subcase_0.case.update.recurrence_count')),
+  field('gciclubfoot__Relapse_Feet_Affected__c', humanProper(state.data.form.subcase_0.case.update.recurrence_feet_affected)), // picklist
+  field('gciclubfoot__Relapse_Type_Left__c', (state) => {
+    const ms = state.data.form.subcase_0.case.update.recurrence_type_left
+    if (ms) {
+      return ms.replace(/ /gi, ';').toLowerCase().split(';').map((value) => {
+        return humanProper(value)
+      }).join(';');
+    } else { return "" }
+  }),
+  field('gciclubfoot__Relapse_Type_Right__c', (state) => {
+    const ms = state.data.form.subcase_0.case.update.recurrence_type_right
+    if (ms) {
+      return ms.replace(/ /gi, ';').toLowerCase().split(';').map((value) => {
+        return humanProper(value)
+      }).join(';');
+    } else { return "" }
+  }),
+  // ===========================================================================
+
+  // Referral Questions ========================================================
+   field('gciclubfoot__Date_Referral_Made__c', (state) => {
+    const validDate = state.data.form.subcase_0.case.update.date_referral_made
+    return ( validDate ? new Date(validDate).toISOString() : null )
+  }),
+  field('gciclubfoot__Referral_Hospital__c', dataValue('form.subcase_0.case.update.referral_hospital')),
+  field('gciclubfoot__Referral_Provider__c', dataValue('form.subcase_0.case.update.referral_provider')),
+  field('gciclubfoot__Referral_Surgery_Type__c', humanProper(state.data.form.subcase_0.case.update.referral_surgery_type)), // picklist
+  field('gciclubfoot__Referral_Surgery_Type_Other__c', dataValue('form.subcase_0.case.update.referral_surgery_type_other')),
+  field('gciclubfoot__Referral_Treatment_Date__c', (state) => {
+    const validDate = state.data.form.subcase_0.case.update.referral_treatment_date
+    return ( validDate ? new Date(validDate).toISOString() : null )
+  }),
+  field('gciclubfoot__Referral_Type__c', humanProper(state.data.form.subcase_0.case.update.referral_type)), // picklist
+  field('gciclubfoot__Referral_Advanced_Care_Specified__c', dataValue('form.subcase_0.case.update.referral_advanced_care')),
+  // ===========================================================================
+
+  // Tenotomy Questions ========================================================
+  field('gciclubfoot__Date_of_Tenotomy__c', (state) => {
+    const validDate = state.data.form.subcase_0.case.update.date_tenotomy
+    return ( validDate ? new Date(validDate).toISOString() : null )
+  }),
+  field('gciclubfoot__Tenotomy_Given__c', humanProper(state.data.form.subcase_0.case.update.tenotomy_given)), // picklist
+  field('gciclubfoot__Tenotomy_Hospital__c', dataValue('form.subcase_0.case.update.tenotomy_hospital')),
+  field('gciclubfoot__Tenotomy_Provider__c', dataValue('form.subcase_0.case.update.tenotomy_provider')),
+  // ===========================================================================
+
+  field('gciclubfoot__Treatment_Completed__c', (state) => {
+    return (state.data.form.subcase_0.case.update.treatment_completed == "1" ? true : false) // sf checkbox
+  }),
+  field('gciclubfoot__Visit_Count__c', dataValue('form.subcase_0.case.update.visit_count')),
+  field('Name', (state) => {
+    return state.data.form.subcase_0.case.update.patient_name + "/" +
+    state.data.form.subcase_0.case.update.patient_id + "/" +
+    state.data.form.subcase_0.case.update.visit_count
+  }),
+  field('gciclubfoot__Visit_Notes__c', dataValue('form.subcase_0.case.update.visit_notes')),
+  field('gciclubfoot__Treatment_Provider__c', dataValue('form.subcase_0.case.update.treatment_provider')),
+  field("gciclubfoot__Photo_1_URL__c", function(state) {
+    const baseUrl = `https://www.commcarehq.org/a/${state.data.domain}/api/form/attachment/`;
+    const uuid = state.data.metadata.instanceID;
+    const image = state.data.form.photos.photo1;
+    return ( image ? `${baseUrl}${uuid}/${image}` : "" )
+  }),
+  field("gciclubfoot__Photo_2_URL__c", function(state) {
+    const baseUrl = `https://www.commcarehq.org/a/${state.data.domain}/api/form/attachment/`;
+    const uuid = state.data.metadata.instanceID;
+    const image = state.data.form.photos.photo2;
+    return ( image ? `${baseUrl}${uuid}/${image}` : "" )
+  }),
+  field("gciclubfoot__Photo_3_URL__c", function(state) {
+    const baseUrl = `https://www.commcarehq.org/a/${state.data.domain}/api/form/attachment/`;
+    const uuid = state.data.metadata.instanceID;
+    const image = state.data.form.photos.photo3;
+    return ( image ? `${baseUrl}${uuid}/${image}` : "" )
+  })
+));
