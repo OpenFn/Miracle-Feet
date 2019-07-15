2a3b20f1a526a0d83fd11c924ea1fe87d207a40f
katyf2190 <42654395+katyf2190@users.noreply.github.com>
2019-07-15 12:31:48 -0400

    Updating shoe_size syntax

--- add_new_visit.js
+++ add_new_visit.js
@@ -66,8 +66,9 @@ upsert("gciclubfoot__Visit__c", "gciclubfoot__commcare_case_id__c", fields(
       var shoe = '';
       if (typeof mf_brace==='undefined' && typeof mf_shoe==='undefined'){
         shoe='';
-      
       } else if (typeof mf_brace.miraclefeet_shoe_size_india==='undefined' && typeof mf_shoe==='undefined'){
+        shoe='':
+      } else if (typeof mf_brace.miraclefeet_shoe_size_india==='undefined'){
         shoe=mf_shoe.charAt(0).toUpperCase() + mf_shoe.slice(1).replace('_', ' ');
       } else {
         shoe=mf_brace.miraclefeet_shoe_size_india.charAt(0).toUpperCase() + mf_brace.miraclefeet_shoe_size_india.slice(1).replace('_', ' ');
