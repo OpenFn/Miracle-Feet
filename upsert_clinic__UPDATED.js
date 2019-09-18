111c94aef5db457e8cff3f684e3338f4a629f02b
aleksa-krolls <aleksagk@gmail.com>
2019-09-18 15:51:50 +0300

    new job mapping to Account

--- upsert_clinic__UPDATED.js
+++ upsert_clinic__UPDATED.js
@@ -1,1 +1,10 @@
-// Your job goes here.
+// =============================================================================
+// New job to support SF migration to Account
+// Upsert Clinic records in Salesforce when Location "Hospital" records are
+// updated in Commcare.
+// =============================================================================
+upsert("Account", "CAST_Location_ID__c", fields(
+  field('CAST_Location_ID__c', dataValue('location_id')),
+  field('Name', dataValue('name'))
+));
+// Ensure that correct CommCare ID is set in SF as external ID.
