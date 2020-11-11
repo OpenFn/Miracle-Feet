985d0116e5d98532b526da996fcef820ff651f24
Aleksa Krolls <aleksagk@gmail.com>
2020-11-11 23:41:30 +0200

    aleksa@openfn.org's change on OpenFn.org

--- add_new_visit__UPDATED.js
+++ add_new_visit__UPDATED.js
@@ -71,7 +71,8 @@ alterState(state => {
     'test_tanzania_clinic',
     'test_uganda_clinic',
     'test_clinic1',
-    'TestMAJ01' //another?
+    'TestMAJ01', //another?
+    'senegal_test_clinic' //another
   ];
 
   state.dateConverter = function (state, dateString) {
