e6f9655070d1487c1eb889891d22a88b295d16e9
Taylor Downs <downs.taylor@gmail.com>
2020-12-02 09:51:57 +0000

    update converter

--- mappings/converter.js
+++ mappings/converter.js
@@ -14,6 +14,25 @@ const groupBy = (array, key) => {
 };
 
 const result = groupBy(data, 'Name');
+
+Object.keys(result).map(type => {
+  const cleaned = result[type].map(x => {
+    delete x.Name;
+    delete x['# Alert'];
+    x.order = x['# SMS'];
+    delete x['# SMS'];
+    Object.keys(x).map(lang => {
+      if (lang.length <= 3) {
+        console.log(`${lang} is a language key`);
+        x[lang] = x[lang].split(/[{}]+/);
+      }
+      return lang;
+    });
+    return x;
+  });
+  result[type] = cleaned;
+});
+
 fs.writeFileSync(
   './mappings/content-output.json',
   JSON.stringify(result, null, 2)
