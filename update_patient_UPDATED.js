d5e32629a93795030594edc04b660f0f85801955
Aleksa Krolls <aleksagk@gmail.com>
2021-05-17 17:01:19 +0300

    Update update_patient_UPDATED.js

--- update_patient_UPDATED.js
+++ update_patient_UPDATED.js
@@ -103,7 +103,7 @@ alterState(state => {
           'Age_Months_First_Brace__c',
            state => {
             var age = dataValue('properties.age_months_first_brace_rounded')(state); 
-            return age==="0" || age=="" ? "" : age; 
+            return age==="0" || age=="" ? " " : age; 
         }),
         field(
           'Age_Months_Started_Treatment__c',
