d297f88117fe67d5a4980b04c53e538bfcc02273
Taylor Downs <taylor@openfn.org>
2021-06-15 08:23:18 +0100

    elisabeth.richardson18@gmail.com's change on OpenFn.org

--- registerDevice.js
+++ registerDevice.js
@@ -1,4 +1,95 @@
 //Job to update Devices based on new Patient records
+
+  const discardedClinics = [
+    'test_bangladesh',
+    'bol_test',
+    'brazil_test',
+    'cam_test',
+    'bong_test',
+    'brazzaville_test',
+    'ecu_test',
+    'gambia_test',
+    'gambia_test_clinic',
+    'guat_test',
+    'guinea_test_clinic',
+    'hon_test',
+    'uptest',
+    'indonesia_test',
+    'test',
+    'liberia_test_clinic',
+    'madagascar_test_clinic',
+    'madagascar_test',
+    'majunga_test_clinic',
+    'morocco_test',
+    'mya_test',
+    'nepal_test',
+    'nica_prueba',
+    'nigeria_test',
+    'test_nigeria',
+    'par_test',
+    'philippines_test',
+    'sl_test',
+    'test_som',
+    'ss_test',
+    'sri_lanka_test',
+    'test_tanzania_clinic',
+    'test_uganda_clinic',
+    'test_clinic1',
+    'TestMAJ01', 
+    'senegal_test_clinic', 
+    'uganda_test_clinic', 
+    'mali_test',
+    'bangladesh_test_clinic',
+    'bolivia_test_clinic',
+    'brazil_test_clinic',
+    'cambodia_test_clinic',
+    'congo_test_clinic',
+    'ecuador_test_clinic',
+    'gautemala_test_clinic',
+    'guinea_test_clinic',
+    'honduras_test_clinic',
+    'india_test_clinic',
+    'indonesia_test_clinic',
+    'liberia_test_clinic',
+    'madagascar_test_clinic',
+    'mali_test_clinic',
+    'morocco_test_clinic',
+    'myanmar_test_clinic',
+    'nepal_test_clinic',
+    'nicaraugua_test_clinic',
+    'nigeria_test_clinic',
+    'paraguary_test_clinic',
+    'phillipines_test_clinic',
+    'senegal_test_clinic',
+    'sierra_leone_test_clinic',
+    'somalia_test_clinic',
+    'test_somalia',
+    'south_sudan_test_clinic',
+    'sri_lanka_test_clinic',
+    'tanzania_test_clinic',
+    'uganda_test_clinic',
+    'test_clinic1',
+    'test_clinic2',
+    'test_clinic3',
+    'test_clinic4',
+  ];
+
+  state.dateConverter = function (state, dateString) {
+    return dateString ? new Date(dateString).toISOString() : null;
+  };
+
+  return { ...state, discardedClinics };
+});
+
+alterState(state => {
+  const { clinic_code } = state.data.form.case.update;
+  if (state.discardedClinics.includes(clinic_code)) {
+    console.log(
+      'This is a CommCare test clinic. Not uploading data to Salesforce.'
+    );
+
+
+
 upsert('Devices__c', 'Device_ID__c', 
  fields(
   field('Device_ID__c',dataValue('metadata.deviceID')),
