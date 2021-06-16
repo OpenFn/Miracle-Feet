183c739cf5507562ff161bf32a6bfcbc511f7e83
Taylor Downs <taylor@openfn.org>
2021-06-16 16:53:27 +0100

    elisabeth.richardson18@gmail.com's change on OpenFn.org

--- registerDevice.js
+++ registerDevice.js
@@ -1,16 +1,17 @@
 //Job to update Devices based on new Patient records
 upsert(
-  'Devices__c',
-  'Device_ID__c', 
- fields(
-  field('Device_ID__c',dataValue('metadata.deviceID')),
-  relationship(
-    'Clinic_r',
-    'CAST_Location_ID__c',
-    dataValue('form.case.create.owner_id')
+    'Devices__c',
+    'Device_ID__c', 
+  fields(
+    field('Device_ID__c',dataValue('metadata.deviceID')),
+    relationship(
+      'Clinic__r',
+      'CAST_Location_ID__c',
+      dataValue('form.case.create.owner_id')
     ),
     field('CAST_Version__c',dataValue('metadata.app_build_version')),
     field('CommCare_Version__c',dataValue('metadata.commcare_version')),
     field('CommCare_Username__c',dataValue('metadata.username')),
     field('CommCare_User_ID__c',dataValue('metadata.userID')),
 ));
+
