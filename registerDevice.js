6d592022816e10108fa956f9b7c28541a4c13cff
Taylor Downs <taylor@openfn.org>
2021-06-17 16:16:01 +0100

    sele.mthimkhulu@miraclefeet.org's change on OpenFn.org

--- registerDevice.js
+++ registerDevice.js
@@ -5,12 +5,12 @@ upsert(
   fields(
     field('Device_ID__c',dataValue('metadata.deviceID')),
     relationship(
-      'Clinic__r',
+      'Clinic__c',
       'CAST_Location_ID__c',
       dataValue('form.case.create.owner_id')
     ),
     field('CAST_Version__c',dataValue('metadata.app_build_version')),
-    field('CommCare_Version__c',dataValue('form.meta.commcare_version')),
+    field('CommCare_Version__c',dataValue('metadata.commcare_version')),
     field('CommCare_Username__c',dataValue('metadata.username')),
     field('CommCare_User_ID__c',dataValue('metadata.userID'))
 ));
