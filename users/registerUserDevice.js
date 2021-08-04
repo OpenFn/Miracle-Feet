6e42c63a939444e7b10ce6de8c46722b58e23ec4
Taylor Downs <taylor@openfn.org>
2021-08-04 15:07:50 +0100

    elisabeth.richardson18@gmail.com's change on OpenFn.org

--- users/registerUserDevice.js
+++ users/registerUserDevice.js
@@ -1,7 +1,7 @@
 
 upsert(
   'Devices',
-  'Device_ID__c') //replace bracket with comma
+  'Device_ID__c', //replace bracket with comma
   fields(
     //field('Device_ID__c', dataValue('XYZ')), //you need to specify where this unique id comes from if using to upsert
     field('CommCare_User_ID__c', dataValue('id')),
@@ -10,7 +10,7 @@ upsert(
           'CAST_Location_ID__c',
           dataValue('user_data.commcare_location_id')),
     field('SMS_Turned_On__c',dataValue('user_data.send_sms'))
-  )
-//missing closing bracket
+  ))
+
 
       
