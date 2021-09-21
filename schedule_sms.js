1c172bbcdfd157d47bb0dd35a2df851899790d99
Rita Zágoni <zagoni.rita@gmail.com>
2021-09-21 10:36:06 +0300

    rita@openfn.org's change on OpenFn.org

--- schedule_sms.js
+++ schedule_sms.js
@@ -181,7 +181,7 @@ alterState(state => {
     PHI: 'PHI',
   };
 
-  const allowedUsernames = ['umuahia01'];
+  const allowedUsernames = ['umuahia01', 'test_tanzania'];
 
   const { form } = state.data;
   const { calcs, properties, close_reason, intro } = form;
