e55443648aeeca65c078e51ffec5a8b4c4d7e224
Taylor Downs <taylor@openfn.org>
2021-09-28 15:38:02 +0100

    sele.mthimkhulu@miraclefeet.org's change on OpenFn.org

--- upsert_clinic__UPDATED.js
+++ upsert_clinic__UPDATED.js
@@ -6,6 +6,6 @@
 upsert("Account", "CAST_Location_ID__c", fields(
   field('CAST_Location_ID__c', dataValue('location_id')),
   field('Name', dataValue('name')), 
-  field('Country1__c', dataValue('country'))
+  field('Country1__c', dataValue('site_code'))
 ));
 // Ensure that correct CommCare ID is set in SF as external ID.
