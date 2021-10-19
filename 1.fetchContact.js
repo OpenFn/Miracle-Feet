8b981280908ce3eed5ca54f35b89b69f5c857dd3
Rita Zágoni <zagoni.rita@gmail.com>
2021-10-19 10:25:48 +0300

    rita@openfn.org's change on OpenFn.org

--- 1.fetchContact.js
+++ 1.fetchContact.js
@@ -1,1 +1,35 @@
-// Your job goes here.
+fn(state => {
+  const today = new Date();
+  const potentialStartDate = [];
+
+  const setDays = (date, x) => {
+    return new Date(new Date(date).setDate(date.getDate() + x))
+      .toISOString()
+      .split('T')[0];
+  };
+
+  potentialStartDate.push(
+    setDays(today, -180),
+    setDays(today, -360),
+    setDays(today, -540)
+  );
+  return query(
+    `SELECT Name, Clinic_Country__c, Guardian_1_Phone_Number_1__c, CommCare_Case_ID__c FROM Contact WHERE SMS_Treatment__c = 'Bracing Night' AND SMS_Treatment_Start_Date__c in (${potentialStartDate.join(
+      ','
+    )}')`
+  )(state).then(state => {
+    const { records } = state.references[0];
+
+    const contacts = records.map(record => {
+      return {
+        id: record.Id,
+        caseId: record.CommCare_Case_ID__c,
+        Name: record.Name,
+        Country: record.Clinic_Country__c,
+        Phone: record.Guardian_1_Phone_Number_1__c,
+      };
+    });
+
+    return { ...state, contacts };
+  });
+});
