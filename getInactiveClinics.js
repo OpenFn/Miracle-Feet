a96ccbb4068836fcc60cc33af0419383bb144916
Aleksa Krolls <aleksagk@gmail.com>
2021-11-01 11:09:21 +0200

    check contact send_sms

--- getInactiveClinics.js
+++ getInactiveClinics.js
@@ -10,7 +10,7 @@ fn(state => ({
 }));
 
 query(
-  `Select Id, CommCare_Case_ID__c from Contact WHERE Account.Status__c = 'Temporarily Suspended'`
+  `Select Id, CommCare_Case_ID__c from Contact WHERE Send_SMS__c = true AND Account.Status__c = 'Temporarily Suspended'`
 );
 
 fn(state => ({
