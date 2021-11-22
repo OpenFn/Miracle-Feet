d62870e681c443aeb74d16853b9ddb3a9bf2b89f
mamadou <mamadou@openfn.org>
2021-11-22 10:50:23 +0000

    add more potentialStartDate,#93

--- 1.fetchContact.js
+++ 1.fetchContact.js
@@ -9,13 +9,21 @@ fn(state => {
   };
 
   potentialStartDate.push(
+    setDays(today, -90),
     setDays(today, -180),
+    setDays(today, -270),
     setDays(today, -360),
+    setDays(today, -450),
     setDays(today, -540),
+    setDays(today, -630),
     setDays(today, -720),
+    setDays(today, -810),
     setDays(today, -900),
+    setDays(today, -990),
     setDays(today, -1080),
-    setDays(today, -1260)
+    setDays(today, -1170),
+    setDays(today, -1260),
+    setDays(today, -1350)
   );
   return query(
     `SELECT Name, SMS_Treatment_Start_Date__c, Guardian_1_First_Name__c, Pronoun_he_she__c, Pronoun_him_her__c, Pronoun_his_her__c, Clinic_Country__c, Guardian_1_Phone_Number_1__c, CommCare_Case_ID__c 
