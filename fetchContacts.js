b80f19037fe115bfae0db60967bf1453e360695e
mamadou <mamadou@openfn.org>
2022-01-03 14:37:31 +0000

    fetch Reason_Stopped_Treatment__c and Last_Modified_Date_CommCare__c, #103

--- fetchContacts.js
+++ fetchContacts.js
@@ -7,9 +7,9 @@ fn(state => {
   // 5. depending on a specific list of account's status
   return query(
     `SELECT Name, Account.Status__c, Guardian_1_First_Name__c, Pronoun_he_she__c, Pronoun_him_her__c, Pronoun_his_her__c, Clinic_Country__c, Guardian_1_Phone_Number_1__c, CommCare_Case_ID__c, 
-    SMS_Treatment_Start_Date__c, Next_Visit_Date__c, Registration_Date__c, Date_of_First_Visit__c, Last_Visit_Date__c,
+    SMS_Treatment_Start_Date__c, Next_Visit_Date__c, Registration_Date__c, Date_of_First_Visit__c, Last_Visit_Date__c, Last_Modified_Date_CommCare__c
     SMS_Opt_In_II__c, SMS_Opt_In__c, 
-    SMS_Treatment__c, SMS_Original_Treatment__c, Brace_Problems_Type__c
+    SMS_Treatment__c, SMS_Original_Treatment__c, Reason_Stopped_Treatment__c, Brace_Problems_Type__c
       FROM Contact 
       WHERE Send_SMS__c = true`
   )(state).then(state => {
@@ -33,12 +33,14 @@ fn(state => {
         registrationDate: record.Registration_Date__c,
         firstVisitDate: record.Date_of_First_Visit__c,
         lastVisitDate: record.Last_Visit_Date__c,
+        lastModifiedDateCommCare: record.Last_Modified_Date_CommCare__c,
 
         smsOptIn: record.SMS_Opt_In__c,
         smsOptInII: record.SMS_Opt_In_II__c,
 
         treatment: record.SMS_Treatment__c,
         originalTreatment: record.SMS_Original_Treatment__c,
+        reasonStoppedTreatment: record.Reason_Stopped_Treatment__c,
         braceProblemType: record.Brace_Problems_Type__c,
       };
     });
