bf1be1a1125430e8b8c4fdc3a5bf3e7011b9ee32
Rita Zágoni <zagoni.rita@gmail.com>
2022-01-05 17:11:36 +0300

    rita@openfn.org's change on OpenFn.org

--- fetchContacts.js
+++ fetchContacts.js
@@ -7,7 +7,7 @@ fn(state => {
   // 5. depending on a specific list of account's status
   return query(
     `SELECT Name, Account.Status__c, Guardian_1_First_Name__c, Pronoun_he_she__c, Pronoun_him_her__c, Pronoun_his_her__c, Clinic_Country__c, Guardian_1_Phone_Number_1__c, CommCare_Case_ID__c, 
-    SMS_Treatment_Start_Date__c, Next_Visit_Date__c, Registration_Date__c, Date_of_First_Visit__c, Last_Visit_Date__c, Last_Modified_Date_CommCare__c
+    SMS_Treatment_Start_Date__c, Next_Visit_Date__c, Registration_Date__c, Date_of_First_Visit__c, Last_Visit_Date__c, Last_Modified_Date_CommCare__c,
     SMS_Opt_In_II__c, SMS_Opt_In__c, Send_SMS__c,
     SMS_Treatment__c, SMS_Original_Treatment__c, Reason_Stopped_Treatment__c, Brace_Problems_Type__c
       FROM Contact`
