3b5be67820914bdd58108d69dad71c4277a30379
mamadou <mamadou@openfn.org>
2022-01-24 13:32:33 +0000

    check if treatment different from originalTreatment, #108

--- schedule_smsV2.js
+++ schedule_smsV2.js
@@ -313,7 +313,9 @@ fn(state => {
         // DELETION FINAL =========================================================
         // If Last_Modified_Date_CommCare__c > today() - 1...
         if (
-          new Date(lastModifiedDateCommCare) > new Date(setDays(new Date(), -1))
+          new Date(lastModifiedDateCommCare) >
+            new Date(setDays(new Date(), -1)) &&
+          treatment !== originalTreatment
         ) {
           // ...and treatment included in treatmentsList then delete originalTreatment...
           // ...or brace problems type...
