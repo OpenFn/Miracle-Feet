ff581c2b13d096adf4cb3af88c0c398dee132b86
Taylor Downs <taylor@openfn.org>
2022-02-17 08:58:04 +0000

    elisabeth.richardson18@gmail.com's change on OpenFn.org

--- registerDevice.js
+++ registerDevice.js
@@ -74,7 +74,8 @@ alterState(state => {
     'test_clinic2',
     'test_clinic3',
     'test_clinic4',
-    'haiti_test_clinic'
+    'haiti_test_clinic',
+    'peru_test_clinic'
   ];
   const { clinic_code } = state.data.form.case.update;
   if (discardedClinics.includes(clinic_code)) {
