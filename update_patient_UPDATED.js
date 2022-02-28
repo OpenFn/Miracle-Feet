d8bb21e5552fab440149b13b34bd4877428d43e0
Rita Zágoni <zagoni.rita@gmail.com>
2022-02-28 07:21:09 +0300

    rita@openfn.org's change on OpenFn.org

--- update_patient_UPDATED.js
+++ update_patient_UPDATED.js
@@ -297,7 +297,7 @@ fn(state => {
       ),
       field(
         'Guardian_1_Phone_Number_1__c',
-        dataValue('properties.guardian1_phone1')
+        dataValue('properties.guardian1_phone1_full')
       ),
       field(
         'Guardian_1_Phone_Number_2__c',
