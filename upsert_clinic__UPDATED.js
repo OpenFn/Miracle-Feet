ff1ba8821d162b480b083ef0c6bda8e3a77c5a00
Rita Zágoni <zagoni.rita@gmail.com>
2022-03-02 07:59:37 +0300

    rita@openfn.org's change on OpenFn.org

--- upsert_clinic__UPDATED.js
+++ upsert_clinic__UPDATED.js
@@ -3,11 +3,15 @@
 // Upsert Clinic records in Salesforce when Location "Hospital" records are
 // updated in Commcare.
 // =============================================================================
-const testClinic = state.data.metadata;
-console.log.testClinic;
-upsert("Account", "CAST_Location_ID__c", fields(
-  field('CAST_Location_ID__c', dataValue('location_id')),
-  field('Name', dataValue('name')), 
-  field('Country1__c', dataValue('country'))
-));
+alterState(state => {
+  const testClinic = state.data.metadata;
+  console.log.testClinic;
+  return upsert("Account", "CAST_Location_ID__c", fields(
+    field('CAST_Location_ID__c', dataValue('location_id')),
+    field('Name', dataValue('name')), 
+    field('Country1__c', dataValue('country'))
+    (state)
+  ));
+  }
+)
 // Ensure that correct CommCare ID is set in SF as external ID.
