1b1e7df5dff03778435dcdf47ce96e80e26cb83b
Rita Zágoni <zagoni.rita@gmail.com>
2022-03-02 08:05:00 +0300

    rita@openfn.org's change on OpenFn.org

--- upsert_clinic__UPDATED.js
+++ upsert_clinic__UPDATED.js
@@ -6,12 +6,17 @@
 alterState(state => {
   const testClinic = state.data.metadata;
   console.log(testClinic);
-  return upsert("Account", "CAST_Location_ID__c", fields(
+  if (testClinic  === "Yes") {
+    console.log(
+      'This is a CommCare test clinic. Not uploading data to Salesforce.'
+    );
+    return state;
+  } else { return upsert("Account", "CAST_Location_ID__c", fields(
     field('CAST_Location_ID__c', dataValue('location_id')),
     field('Name', dataValue('name')), 
     field('Country1__c', dataValue('country'))
-    (state)
-  ));
+   )) (state);
+    }
   }
 )
 // Ensure that correct CommCare ID is set in SF as external ID.
