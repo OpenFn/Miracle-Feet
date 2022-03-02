85667ad3058ac5c22f3cecbe847e77d3310cf061
Rita Zágoni <zagoni.rita@gmail.com>
2022-03-02 08:00:57 +0300

    rita@openfn.org's change on OpenFn.org

--- upsert_clinic__UPDATED.js
+++ upsert_clinic__UPDATED.js
@@ -5,7 +5,7 @@
 // =============================================================================
 alterState(state => {
   const testClinic = state.data.metadata;
-  console.log.testClinic;
+  console.log(testClinic);
   return upsert("Account", "CAST_Location_ID__c", fields(
     field('CAST_Location_ID__c', dataValue('location_id')),
     field('Name', dataValue('name')), 
