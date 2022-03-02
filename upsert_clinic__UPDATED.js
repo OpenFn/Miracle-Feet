163de6a37dd2f8fc8c95761eb3583f495de7ca50
Rita Zágoni <zagoni.rita@gmail.com>
2022-03-02 08:16:04 +0300

    rita@openfn.org's change on OpenFn.org

--- upsert_clinic__UPDATED.js
+++ upsert_clinic__UPDATED.js
@@ -5,9 +5,7 @@
 // =============================================================================
 alterState(state => {
   const { test_clinic } = state.data.metadata;
-  console.log(test_clinic);
-  console.log(test_clinic  === 'Yes');
-  if (test_clinic  == 'Yes') {
+  if (test_clinic  === 'Yes') {
     console.log(
       'This is a CommCare test clinic. Not uploading data to Salesforce.'
     );
