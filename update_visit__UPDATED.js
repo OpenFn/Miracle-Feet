9eaf079b505087f4607d1cf1cbbc8b84b7642921
Taylor Downs <taylor@openfn.org>
2023-04-18 10:53:48 +0100

    akkaouim@gmail.com's change on OpenFn.org

--- update_visit__UPDATED.js
+++ update_visit__UPDATED.js
@@ -179,7 +179,7 @@ alterState(state => {
   const { clinic_code, patient_id } = state.data.properties;
   if (
       state.discardedClinics.includes(clinic_code) || 
-      state.data.form.case.update.test_user == 'Yes' ||
+      state.data.properties.test_user == 'Yes' ||
       state.discardedCountries.includes(dataValue('state.data.properties.patient_country')(state))
       ) {
     console.log(
